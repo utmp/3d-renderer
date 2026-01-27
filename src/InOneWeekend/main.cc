4cd32e2af99d2ab9d42e5a8030db8afd4341fb75
utmp <2210206543@ogrenci.karabuk.edu.tr>
2026-01-27 18:49:51 +0300

    implemented vec3 and color

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -1,7 +1,9 @@
+#include "color.h"
+#include "vec3.h"
 #include <iostream>
 
 int main(){
-    
+    // img resolution
     int img_width = 256;
     int img_height = 256;
 
@@ -12,15 +14,8 @@ int main(){
     for (int j = 0; j<img_height; ++j){
         std::clog << "\rScanlines remaining: " << (img_height -j) << ' ' << std::flush;
         for(int i = 0;i<img_width;i++){
-            auto r = double(i) / (img_width-1);
-            auto g = double(j) / (img_height-1);
-            auto b = 0.0;
-
-            int ir = int(255.999 * r);
-            int ig = int(255.999 * g);
-            int ib = int(255.999 * b);
-
-            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+           auto pixel_color = color(double(i)/(img_width-1), double(j)/(img_height-1),0);
+           write_color(std:: cout,pixel_color);
         }
     }
     std::clog << "\rDone.                      \n"; 
