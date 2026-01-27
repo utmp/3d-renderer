97afc82bb86458f7d0530fec2335ee3d3f5c94c6
utmp <2210206543@ogrenci.karabuk.edu.tr>
2026-01-27 13:38:44 +0300

    initial commit

---
+++ src/InOneWeekend/main.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main(){
+    
+    int img_width = 256;
+    int img_height = 256;
+
+    // rendering
+
+    std::cout << "P3\n" << img_width << ' ' <<  img_height << "\n255\n";
+
+    for (int j = 0; j<img_height; j++){
+        for(int i = 0;i<img_width;i++){
+            auto r = double(i) / (img_width-1);
+            auto g = double(j) / (img_height-1);
+            auto b = 0.0;
+
+            int ir = int(255.999 * r);
+            int ig = int(255.999 * g);
+            int ib = int(255.999 * b);
+
+            std::cout << ir << ' ' << ig << ' ' << ib << '\n';
+        }
+    }
+}
