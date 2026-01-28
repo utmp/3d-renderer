db209031cad14a6ae870e5e859c8cda32882f231
utmp <2210206543@ogrenci.karabuk.edu.tr>
2026-01-28 09:41:54 +0300

    implemented ray

--- src/InOneWeekend/main.cc
+++ src/InOneWeekend/main.cc
@@ -1,11 +1,54 @@
 #include "color.h"
 #include "vec3.h"
+#include "ray.h"
 #include <iostream>
+
+double hit_sphere(const point3& center,double radius, const ray& r){
+    vec3 oc = center - r.origin();
+    auto a = dot(r.direction(),r.direction());
+    auto b = -2.0 * dot(r.direction(),oc);
+    auto c = dot(oc,oc) - radius*radius;
+    auto discriminant = b*b - 4*a*c;
+    if(discriminant<0){
+        return -1.0;
+    }else{
+        return (-b - std::sqrt(discriminant)) / (2.0*a);
+    }
+}
+
+color ray_color(const ray& r){
+    auto t = hit_sphere(point3(0,0,-1),0.5,r);
+    if(t>0.0){
+        vec3 N = unit_vector(r.at(t) - vec3(0,0,-1));
+        return 0.5*color(N.x()+1, N.y()+1,N.z()+1);
+    }
+
+    vec3 unit_direction = unit_vector(r.direction());
+    auto a = 0.5*(unit_direction.y() + 1.0);
+    return (1.0 - a)*color(1.0,1.0,1.0) + a*color(0.5,0.7,1.0);
+}
 
 int main(){
     // img resolution
-    int img_width = 256;
-    int img_height = 256;
+    auto aspect_ratio = 16.0 / 9.0;
+    int img_width = 400;
+    int img_height = int(img_width /aspect_ratio);
+    img_height = (img_height<1) ? 1 : img_height;
+
+    // camera
+    auto focal_length = 1.0;
+    auto viewport_height = 2.0;
+    auto viewport_width = viewport_height * double((img_width)/img_height);
+    auto camera_center = point3(0,0,0);
+
+    auto viewport_u = vec3(viewport_width,0,0);
+    auto viewport_v = vec3(0,-viewport_height,0);
+
+    auto pixel_delta_u = viewport_u / img_width;
+    auto pixel_delta_v = viewport_v / img_height;
+
+    auto viewport_upper_left = camera_center - vec3(0,0,focal_length) - viewport_u/2 - viewport_v/2;
+    auto pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
 
     // rendering
 
@@ -14,8 +57,12 @@ int main(){
     for (int j = 0; j<img_height; ++j){
         std::clog << "\rScanlines remaining: " << (img_height -j) << ' ' << std::flush;
         for(int i = 0;i<img_width;i++){
-           auto pixel_color = color(double(i)/(img_width-1), double(j)/(img_height-1),0);
-           write_color(std:: cout,pixel_color);
+           auto pixel_center = pixel00_loc + (i* pixel_delta_u) + (j*pixel_delta_v);
+           auto ray_direction = pixel_center - camera_center;
+           ray r(camera_center,ray_direction);
+
+           color pixel_color = ray_color(r);
+           write_color(std::cout,pixel_color);
         }
     }
     std::clog << "\rDone.                      \n"; 
